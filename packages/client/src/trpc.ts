b1a138b155fdb6927912fd227a0ea430bf89dbd4
ryota takaoka <takaokar19911013t@gmail.com>
2026-03-17 19:12:01 +0900

    chore(client): Reorder imports in trpc client

--- packages/client/src/trpc.ts
+++ packages/client/src/trpc.ts
@@ -1,7 +1,7 @@
 import type { AppRouter } from "@dcc/server/trpc";
-import type { CreateTRPCReact } from "@trpc/react-query";
 import type { CreateTRPCClient } from "@trpc/client";
 import { httpBatchLink, httpSubscriptionLink, splitLink } from "@trpc/client";
+import type { CreateTRPCReact } from "@trpc/react-query";
 import { createTRPCReact } from "@trpc/react-query";
 
 export const trpc: CreateTRPCReact<AppRouter, unknown> = createTRPCReact<AppRouter>();
